f71ef17c4f21206b7968d237a0c765695c454db7
paulranjan694 <paulranjan694@gmail.com>
2021-06-21 18:25:27 +0530

    Link Updated

--- homepage.js
+++ homepage.js
@@ -1,9 +1,13 @@
 const request = require("request");
 const chalk = require("chalk");
+const cheerio = require("cheerio");
 
 const fs = require("fs");
 
-let link = "https://www.espncricinfo.com/series/icc-world-test-championship-2019-2021-1195334";
+const getAllMatches = require("./allMatches");
+
+
+let link = "https://www.espncricinfo.com/series/icc-cricket-world-cup-2019-1144415";
 
 request(link, cb);
 
@@ -18,5 +22,10 @@ function cb(error, response, html){
 }
 
 function parseData(html){
-    
+    let ch = cheerio.load(html);
+    let aTag = ch(".widget-items.cta-link a").attr("href");
+    let completeLink = "https://www.espncricinfo.com"+aTag;
+    // console.log(completeLink);
+
+    getAllMatches(completeLink);
 }
